9fc9501d16359bc5f97d237f68d89b86bcfb66f4
姚骏 <11@11.com>
2018-10-11 18:09:20 +0800

    更新配置和批处理

---
+++ cleanup_appchain.bat
@@ -0,0 +1,15 @@
+rd root1\AppChain_46650ecd479d802040de34124b35fc3be16f1800 /s /q
+rd root1\ApplicationLogs_74746E41_00186fe13bfc354b1234de4020809d47cd0e6546 /s /q
+
+rd root2\AppChain_46650ecd479d802040de34124b35fc3be16f1800 /s /q
+rd root2\ApplicationLogs_74746E41_00186fe13bfc354b1234de4020809d47cd0e6546 /s /q
+
+rd root3\AppChain_46650ecd479d802040de34124b35fc3be16f1800 /s /q
+rd root3\ApplicationLogs_74746E41_00186fe13bfc354b1234de4020809d47cd0e6546 /s /q
+
+
+rd root4\AppChain_46650ecd479d802040de34124b35fc3be16f1800 /s /q
+rd root4\ApplicationLogs_74746E41_00186fe13bfc354b1234de4020809d47cd0e6546 /s /q
+
+
+pause
--- startup.bat
+++ startup.bat
@@ -1,11 +1,14 @@
 cd root1
 start startup.bat
+timeout /t 3
 
 cd ..\root2
 start startup.bat
+timeout /t 3
 
 cd ..\root3
 start startup.bat
+timeout /t 3
 
 cd ..\root4
 start startup.bat
