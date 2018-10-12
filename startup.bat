9536de355134d35fb502e4658119ff9adeee5e86
姚骏 <11@11.com>
2018-10-12 17:02:59 +0800

    更新配置

--- startup.bat
+++ startup.bat
@@ -1,14 +1,11 @@
 cd root1
 start startup.bat
-timeout /t 3
 
 cd ..\root2
 start startup.bat
-timeout /t 3
 
 cd ..\root3
 start startup.bat
-timeout /t 3
 
 cd ..\root4
 start startup.bat
---
+++ updatePlugins.bat
@@ -0,0 +1,43 @@
+copy ..\Zoro-Plugins\ApplicationLogs\bin\Debug\netstandard2.0\ApplicationLogs.dll .\
+copy ..\Zoro-Plugins\ApplicationLogs\bin\Debug\netstandard2.0\ApplicationLogs\config.json .\
+
+copy ApplicationLogs.dll .\root1\Plugins
+copy ApplicationLogs.dll .\root2\Plugins
+copy ApplicationLogs.dll .\root3\Plugins
+copy ApplicationLogs.dll .\root4\Plugins
+
+mkdir .\root1\Plugins\ApplicationLogs
+mkdir .\root2\Plugins\ApplicationLogs
+mkdir .\root3\Plugins\ApplicationLogs
+mkdir .\root4\Plugins\ApplicationLogs
+
+copy config.json .\root1\Plugins\ApplicationLogs\
+copy config.json .\root2\Plugins\ApplicationLogs\
+copy config.json .\root3\Plugins\ApplicationLogs\
+copy config.json .\root4\Plugins\ApplicationLogs\
+
+del ApplicationLogs.dll
+del config.json
+
+copy ..\Zoro-Plugins\SimplePolicy\bin\Debug\netstandard2.0\SimplePolicy.dll .\
+copy ..\Zoro-Plugins\SimplePolicy\bin\Debug\netstandard2.0\SimplePolicy\config.json .\
+
+copy SimplePolicy.dll .\root1\Plugins
+copy SimplePolicy.dll .\root2\Plugins
+copy SimplePolicy.dll .\root3\Plugins
+copy SimplePolicy.dll .\root4\Plugins
+
+mkdir .\root1\Plugins\SimplePolicy
+mkdir .\root2\Plugins\SimplePolicy
+mkdir .\root3\Plugins\SimplePolicy
+mkdir .\root4\Plugins\SimplePolicy
+
+copy config.json .\root1\Plugins\SimplePolicy\
+copy config.json .\root2\Plugins\SimplePolicy\
+copy config.json .\root3\Plugins\SimplePolicy\
+copy config.json .\root4\Plugins\SimplePolicy\
+
+del SimplePolicy.dll
+del config.json
+
+pause
