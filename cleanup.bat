9ba6a437df821af1e2db28ded1c57ea5dab3c6d7
lizhan <joshua0526@163.com>
2018-10-19 17:41:57 +0800

    修改Log，AppChain，ApplicationLogs目录结构，修改cleanup.bat和cleanup_appchain.bat

--- cleanup.bat
+++ cleanup.bat
@@ -1,21 +1,21 @@
 rd root1\Chain_74746E41 /s /q
 rd root1\Index_74746E41 /s /q
-rd root1\Logs_0000000000000000000000000000000000000000 /s /q
-rd root1\ApplicationLogs_74746E41_0000000000000000000000000000000000000000 /s /q
+rd root1\Logs /s /q
+rd root1\ApplicationLogs /s /q
 
 rd root2\Chain_74746E41 /s /q
 rd root2\Index_74746E41 /s /q
-rd root2\Logs_0000000000000000000000000000000000000000 /s /q
-rd root2\ApplicationLogs_74746E41_0000000000000000000000000000000000000000 /s /q
+rd root2\Logs /s /q
+rd root2\ApplicationLogs /s /q
 
 rd root3\Chain_74746E41 /s /q
 rd root3\Index_74746E41 /s /q
-rd root3\Logs_0000000000000000000000000000000000000000 /s /q
-rd root3\ApplicationLogs_74746E41_0000000000000000000000000000000000000000 /s /q
+rd root3\Logs /s /q
+rd root3\ApplicationLogs /s /q
 
 rd root4\Chain_74746E41 /s /q
 rd root4\Index_74746E41 /s /q
-rd root4\Logs_0000000000000000000000000000000000000000 /s /q
-rd root4\ApplicationLogs_74746E41_0000000000000000000000000000000000000000 /s /q
+rd root4\Logs /s /q
+rd root4\ApplicationLogs /s /q
 
 pause
