b6587e8fd5c3708053ff2a6469455bc6499f1db6
Michael Yao <michaely2008@sina.com>
2018-11-05 21:39:44 +0800

    提交更新

--- cleanup.bat
+++ cleanup.bat
@@ -1,21 +1,21 @@
 rd root1\Chain_0378734A /s /q
 rd root1\Index_0378734A /s /q
-rd root1\Logs_0000000000000000000000000000000000000000 /s /q
-rd root1\ApplicationLogs_0378734A_0000000000000000000000000000000000000000 /s /q
+rd root1\Logs /s /q
+rd root1\ApplicationLogs /s /q
 
 rd root2\Chain_0378734A /s /q
 rd root2\Index_0378734A /s /q
-rd root2\Logs_0000000000000000000000000000000000000000 /s /q
-rd root2\ApplicationLogs_0378734A_0000000000000000000000000000000000000000 /s /q
+rd root2\Logs /s /q
+rd root2\ApplicationLogs /s /q
 
 rd root3\Chain_0378734A /s /q
 rd root3\Index_0378734A /s /q
-rd root3\Logs_0000000000000000000000000000000000000000 /s /q
-rd root3\ApplicationLogs_0378734A_0000000000000000000000000000000000000000 /s /q
+rd root3\Logs /s /q
+rd root3\ApplicationLogs /s /q
 
 rd root4\Chain_0378734A /s /q
 rd root4\Index_0378734A /s /q
-rd root4\Logs_0000000000000000000000000000000000000000 /s /q
-rd root4\ApplicationLogs_0378734A_0000000000000000000000000000000000000000 /s /q
+rd root4\Logs /s /q
+rd root4\ApplicationLogs /s /q
 
 pause
--- startup.bat
+++ startup.bat
@@ -1,14 +1,11 @@
 cd root1
 start startup.bat
-timeout /t 1
 
 cd ..\root2
 start startup.bat
-timeout /t 1
 
 cd ..\root3
 start startup.bat
-timeout /t 1
 
 cd ..\root4
 start startup.bat
