f9484fdaabe0fef0c5d739f70dfb9394fa7b9ab8
Michael Yao <michaely2008@sina.com>
2018-11-07 22:12:32 +0800

    更新dll

--- cleanup.bat
+++ cleanup.bat
@@ -2,20 +2,24 @@ rd root1\Chain_0378734A /s /q
 rd root1\Index_0378734A /s /q
 rd root1\Logs /s /q
 rd root1\ApplicationLogs /s /q
+del root1\error.log
 
 rd root2\Chain_0378734A /s /q
 rd root2\Index_0378734A /s /q
 rd root2\Logs /s /q
 rd root2\ApplicationLogs /s /q
+del root2\error.log
 
 rd root3\Chain_0378734A /s /q
 rd root3\Index_0378734A /s /q
 rd root3\Logs /s /q
 rd root3\ApplicationLogs /s /q
+del root3\error.log
 
 rd root4\Chain_0378734A /s /q
 rd root4\Index_0378734A /s /q
 rd root4\Logs /s /q
 rd root4\ApplicationLogs /s /q
+del root4\error.log
 
 pause
